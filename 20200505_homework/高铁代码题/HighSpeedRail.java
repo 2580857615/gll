57c1a87084dce0fe9fdf9452fb89ab0559e06c09
2580857615 <2580857615@qq.com>
2020-05-06 09:43:34 +0800

    高铁代码的优化

--- 20200505_homework/高铁代码题/HighSpeedRail.java
+++ 20200505_homework/高铁代码题/HighSpeedRail.java
@@ -9,7 +9,7 @@ public class HighSpeedRail {
     	/* 用一个数组来表示高铁票,数组的第一个元素表示几等座,第二个元素表示终点站,这里还可以再创建一个元素来表示起点站
     	 * ,考虑到本例并没有用到起点站这个元素,所以并没有创建了,默认起点站南京
     	 */
-		String[] selection = {"二等座","江"};
+		String[] selection = {"二等座","南京"};
 		//数组元素随着终点站和几等座位的变化而变化着
 		printPrice(selection, seatKing, stationPlace, priceOne, priceTwo);
 	}
@@ -18,43 +18,42 @@ public class HighSpeedRail {
 		if (selection[0].equals("一等座")) {
 			if (selection[1].equals(stationPlace[0])) {
 				System.out.println("对不起,您输入的终点站与起点站是同一站,请重新输入终点站");
-			}else if (selection[1].equals(stationPlace[1])) {
-				System.out.println(priceOne[1]);
-			}else if (selection[1].equals(stationPlace[2])) {
-				System.out.println(priceOne[2]);
-			}else if (selection[1].equals(stationPlace[3])) {
-				System.out.println(priceOne[3]);
-			}else if (selection[1].equals(stationPlace[4])) {
-				System.out.println(priceOne[4]);
-			}else if (selection[1].equals(stationPlace[5])) {
-				System.out.println(priceOne[5]);
-			}else if (selection[1].equals(stationPlace[6])) {
-				System.out.println(priceOne[6]);
 			}else {
-				System.out.println("对不起,您选择的站不在本次列车行径站中,请在以下站点重新选择:");
-				for(int i=0;i<=stationPlace.length;i++){
-					System.out.print(stationPlace[i]+" ");
+				/*flag用来作为一个标记,当for循环遍历一遍stationPlace后没有找到与selection中对应的站点,
+				则可以判断selection所选不在staionPlace中
+				 */
+				int flag = 1;
+				for(int i=1;i<stationPlace.length;i++){
+					if (selection[1].equals(stationPlace[i])) {
+						System.out.println(priceOne[i]);
+						break;
+					}
+					flag++;
+				}
+				if (flag==stationPlace.length) {
+					System.out.println("对不起,您选择的站不在本次列车行径站中,请在以下站点重新选择:");
+					for(int j=0;j<stationPlace.length;j++){
+						System.out.print(stationPlace[j]+" ");
+				    }
 				}
 			}
 		}else if (selection[0].equals("二等座")) {
 			if (selection[1].equals(stationPlace[0])) {
 				System.out.println("对不起,您输入的终点站与起点站是同一站,请重新输入终点站");
-			}else if (selection[1].equals(stationPlace[1])) {
-				System.out.println(priceTwo[1]);
-			}else if (selection[1].equals(stationPlace[2])) {
-				System.out.println(priceTwo[2]);
-			}else if (selection[1].equals(stationPlace[3])) {
-				System.out.println(priceTwo[3]);
-			}else if (selection[1].equals(stationPlace[4])) {
-				System.out.println(priceTwo[4]);
-			}else if (selection[1].equals(stationPlace[5])) {
-				System.out.println(priceTwo[5]);
-			}else if (selection[1].equals(stationPlace[6])) {
-				System.out.println(priceTwo[6]);
 			}else {
-				System.out.println("对不起,您选择的站不在本次列车行径站中,请在以下站点重新选择:");
-				for(int i=0;i<stationPlace.length;i++){
-					System.out.print(stationPlace[i]+" ");
+				int flag = 1;
+				for(int i=1;i<stationPlace.length;i++){
+					if (selection[1].equals(stationPlace[i])) {
+						System.out.println(priceTwo[i]);
+						break;
+					}
+					flag++;
+				}
+				if (flag==stationPlace.length) {
+					System.out.println("对不起,您选择的站不在本次列车行径站中,请在以下站点重新选择:");
+					for(int j=0;j<stationPlace.length;j++){
+						System.out.print(stationPlace[j]+" ");
+				    }
 				}
 			}
 		}else {
